d912c018b01b128edfb10cbaacdbd3ae8cbf7119
Bob <example@gmail.com>
2022-07-08 08:53:51 +0200

    Fix the code

--- foobar/__main__.py
+++ foobar/__main__.py
@@ -4,5 +4,10 @@ import hydra
 @hydra.main(config_path=Path(__file__).parent.parent / "conf", config_name="config")
 def main(cfg) -> None:
     print(f"Hello {cfg.name} {cfg.surname}!")
+    raise IOError(
+        "This is a test exception to check if we can see why our pipeline fails.\n"
+        f"Got the following params name={cfg.name}, surname={cfg.surname}"
+    )
+
 if __name__ == "__main__":
     main()
